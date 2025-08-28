7311c4f3df23167c1feed04a4de725d92b146070
haruki-mtn <haruki.mtn@gmail.com>
2025-08-28 15:06:13 +0900

    routes.rbのシンタックスエラーを解消

--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,33 @@
 Rails.application.routes.draw do
 
-  scope '(:locale)', locale: /#{I18n.available_locales.map(&:to_s).join('|')}/ , defaults: { locale: "ja" } do  
+  scope '(:locale)', locale: /ja|en|ko|zh/, defaults: { locale: "ja" } do
 
-  get 'password_resets/new'
-  get 'password_resets/edit'
-  root   "static_pages#home"
-  get    "/help",    to: "static_pages#help"
-  get    "/about",   to: "static_pages#about"
-  get    "/contact", to: "static_pages#contact"
-  get    "/signup",  to: "users#new"
-  get    "/login",   to: "sessions#new"
-  post   "/login",   to: "sessions#create"
-  delete "/logout",  to: "sessions#destroy"
-  resources :users do
-    member do
-      get :following, :followers
+    get 'password_resets/new'
+    get 'password_resets/edit'
+    root   "static_pages#home"
+    get    "/help",    to: "static_pages#help"
+    get    "/about",   to: "static_pages#about"
+    get    "/contact", to: "static_pages#contact"
+    get    "/signup",  to: "users#new"
+    get    "/login",   to: "sessions#new"
+    post   "/login",   to: "sessions#create"
+    delete "/logout",  to: "sessions#destroy"
+    resources :users do
+      member do
+        get :following, :followers
+      end
     end
+    resources :account_activations, only: [:edit]
+    resources :password_resets,     only: [:new, :create, :edit, :update]
+    resources :microposts,          only: [:create, :destroy] do
+      collection do
+        get :latest
+      end
+      member do
+        post :pin
+      end
+    end
+    resources :relationships,       only: [:create, :destroy]
+    get '/microposts', to: 'static_pages#home'
   end
-  resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :microposts,          only: [:create, :destroy] do
-    collection do
-      get :latest
-    end
-    member do
-      post :pin
-    end
-  end
-  resources :relationships,       only: [:create, :destroy]
-  get '/microposts', to: 'static_pages#home'
 end
